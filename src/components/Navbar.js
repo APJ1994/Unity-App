5ab9a3ff8daf9fff0b61d9cecd822ff4a236969d
Aman <amanprakashjanoriya@gmail.com>
2023-05-02 13:37:05 +0530

    new navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,6 +21,12 @@ export default function Navbar(props) {
         </li>
        
       </ul>
+
+      <div className="d-flex">
+        <div className="bg-primary rounded mx-2">
+
+        </div>
+      </div>
       {/* <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
